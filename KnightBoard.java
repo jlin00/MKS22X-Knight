4015b586f07273bae1c1ad7afba5ca710b7f1323
Jackie Lin <jlin00@stuy.edu>
2019-02-12 11:12:44 -0500

    added dummy methods

---
+++ KnightBoard.java
@@ -0,0 +1,34 @@
+public class KnightBoard{
+  private int[][] board;
+  private int[][] moves;
+
+  public KnightBoard(){}
+
+  public String toString(){
+    return "";
+  }
+
+  /**
+  *@throws IllegalStateException when the board contains non-zero values.
+  *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  */
+  public boolean solve(int startingRow, int startingCol){
+    return true;
+  }
+
+  //helper method for solve
+  private boolean solveH(int row ,int col, int level){
+    return true;
+  }
+
+  /**
+  *@throws IllegalStateException when the board contains non-zero values.
+  *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  */
+  public int countSolutions(int startingRow, int startingCol){
+    return 0;
+  }
+
+
+
+}
