0df7f6369568c707cd2cdcf879a4150d6f9811ec
Jackie Lin <jlin00@stuy.edu>
2019-02-14 09:38:45 -0500

    added new exception testing

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,8 +3,9 @@ public class KnightBoard{
   private int[][] positions; //keeps track of potential positions for optimization
 
   public KnightBoard(int startingRows, int startingCols){
+    if (startingRows <= 0 || startingCols <= 0) throw new IllegalArgumentException();
     board = new int[startingRows][startingCols];
-    moves = new int[startingRows][startingCols];
+    positions = new int[startingRows][startingCols];
   }
 
   public String toString(){
@@ -19,17 +20,29 @@ public class KnightBoard{
     return output;
   }
 
+  public boolean isEmpty(){
+    for (int r = 0; r < board.length; r++){
+      for (int c = 0; c < board[r].length; c++){
+        if (board[r][c] != 0) return false;
+      }
+    }
+    return true;
+  }
+
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
   */
   public boolean solve(int startingRow, int startingCol){
-    return solveH(startingRow,startingCol,1);
+    if (!isEmpty()) throw new IllegalStateException(); //exception
+    return solveH(startingRow,startingCol,1); //call helper
   }
 
   //helper method for solve
   private boolean solveH(int row ,int col, int level){
-    int[][] diff = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}}; //8 possible moves for a knight 
+    int[][] diff = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}}; //8 possible moves for a knight
+    if (level > row * col) return true;
+    return false;
   }
 
   /**
@@ -37,6 +50,7 @@ public class KnightBoard{
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
   */
   public int countSolutions(int startingRow, int startingCol){
+    if (!isEmpty()) throw new IllegalStateException(); //exception
     return 0;
   }
 
