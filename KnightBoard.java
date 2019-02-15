e48e7b72508c46301435514cfbe7bf46aff354f0
Jackie Lin <jlin00@stuy.edu>
2019-02-14 22:16:50 -0500

    added new move method to make helper methods shorter

--- KnightBoard.java
+++ KnightBoard.java
@@ -31,6 +31,13 @@ public class KnightBoard{
     return true;
   }
 
+  public boolean move(int row, int col, int level){
+    if (row < 0 || col < 0 || row >= board.length || col >= board[row].length) return false; //if any moves go out of bounds return false
+    if (board[row][col] != 0) return false; //if knight has already been here, return false
+    board[row][col] = level; //else move knight here
+    return true;
+  }
+
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
@@ -46,17 +53,11 @@ public class KnightBoard{
   //helper method for solve
   private boolean solveH(int row, int col, int level){
     if (level > (board.length * board[0].length)) return true; //if filled up board, return true
-
-    if (row < 0 || col < 0 || row >= board.length || col >= board[row].length){
-      return false; //if any moves go out of bounds return false
-    }
-
-    if (board[row][col] != 0) return false; //if knight has already been to that position
-
     for (int i = 0; i < diff.length; i++){ //loop through possible moves
-      board[row][col] = level;
-      if (solveH(row + diff[i][0], col + diff[i][1],level+1)) return true; //try moving knight
-      board[row][col] = 0; //try different position instead
+      if (move(row,col,level)){
+        if (solveH(row + diff[i][0], col + diff[i][1],level+1)) return true; //try moving knight
+        board[row][col] = 0; //try different position instead
+      }
     }
     return false;
   }
@@ -69,25 +70,19 @@ public class KnightBoard{
     if (!isEmpty()) throw new IllegalStateException(); //exception
 
     if (startingRow < 0 || startingCol < 0 || startingRow >= board.length || startingCol >= board[startingRow].length){
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException(); //exception
     }
-    return countH(startingRow,startingCol,1);
+    return countH(startingRow,startingCol,1); //helper method
   }
 
   public int countH(int row, int col, int level){
     if (level > (board.length * board[0].length)) return 1;
-
-    if (row < 0 || col < 0 || row >= board.length || col >= board[row].length){
-      return 0; //if any moves go out of bounds return 0
-    }
-
-    if (board[row][col] != 0) return 0; //if knight has already been to that position
-
     int count = 0;
     for (int i = 0; i < diff.length; i++){ //loops through moves
-      board[row][col] = level; //moves knight
-      count += countH(row + diff[i][0], col + diff[i][1], level+1); //adds to count
-      board[row][col] = 0; //removes knight 
+      if (move(row,col,level)){
+        count += countH(row + diff[i][0], col + diff[i][1], level+1); //adds to count
+        board[row][col] = 0; //removes knight
+      }
     }
     return count;
   }
