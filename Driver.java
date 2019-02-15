58673d246bae8bc8ab4e239e22f1b57582332097
Jackie Lin <jlin00@stuy.edu>
2019-02-14 21:49:32 -0500

    added test cases for solve

--- Driver.java
+++ Driver.java
@@ -3,13 +3,21 @@ public class Driver{
     KnightBoard k1 = new KnightBoard(5,6);
     KnightBoard k2 = new KnightBoard(4,3);
     KnightBoard k3 = new KnightBoard(2,2);
-    KnightBoard k4 = new KnightBoard(10,15);
-    KnightBoard k5 = new KnightBoard(3,9);
+    KnightBoard k4 = new KnightBoard(3,9);  
 
     System.out.println(k1);
     System.out.println(k2);
     System.out.println(k3);
     System.out.println(k4);
-    System.out.println(k5);
+
+    System.out.println(k1.solve(0,0));
+    System.out.println(k2.solve(0,0));
+    System.out.println(k3.solve(0,0));
+    System.out.println(k4.solve(0,0));
+
+    System.out.println(k1);
+    System.out.println(k2);
+    System.out.println(k3);
+    System.out.println(k4);
   }
 }
